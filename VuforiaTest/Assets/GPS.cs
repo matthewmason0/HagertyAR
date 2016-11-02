90a05815b8ecc2e95a5ce075a347c8bab0324b9f
MASON <5917000863@student.myscps.us>
2016-11-02 07:58:02 -0400

    updated project from s-drive, with gps.cs

--- VuforiaTest/Assets/GPS.cs
+++ VuforiaTest/Assets/GPS.cs
@@ -11,31 +11,56 @@ public class GPS : MonoBehaviour {
     public float scale;
     public bool FakingLocation;
 
-	void Start () {
+    public void Start()
+    {
         Input.location.Start(0.5f);
         Input.compass.enabled = true;
 
         FInit = Vector2.zero;
-	}
-	
-	IEnumerator UpdatePos () {
-        if (Input.location.isEnabledByUser == false)
-            Debug.Log("Failed because User did not enable location");
+    }
 
-        int maxWait = 20;
-        while (Input.location.status==LocationServiceStatus.Initializing && maxWait>0)
+    IEnumerator UpdatePosition()
+    {
+        if (FakingLocation is false;
         {
-            yield return new WaitForSeconds(1);
-            maxWait--;
+            if (Input.location.isEnabledByUser == false)
+            {
+                Debug.Log("Failed because User did not enable location");
+            }
+
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+            if (maxWait < 1)
+            {
+                Debug.Log("Initializing failed, try again");
+                yield return null;
+            }
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                Debug.Log("LocationInfo Servuse Status Failed!")
+                yield return null;
+            }
+            else
+            {
+                SetLocation(Input.location.lastData.latitude, Input.location.lastData.longitude);
+            }
         }
-        if(maxWait<1)
-        {
-            Debug.Log("Initializing failed, try again");
-            yield return null;
+         else
+         {
+            SetLocation(100 + Time.time, 100 + Time.time);
         }
-        if(Input.location.status == LocationServiceStatus.Failed)
-        {
-            
-        }
-	}
+
+
+    }
+    void SetLocation(float latitude, float longitude)
+    {
+        RCurrentPos = new Vector2(latitude, longitude);
+        Vector2 delta = new Vector2(RCurrentPos.x - RInit.x, RCurrentPos.y - RInit.y);
+        FCurrentPosition = delta * scale;
+        this.transform.position = new Vector3(FCurrentPosition.x, 0, FCurrentPosition.y);
+    }
 }
