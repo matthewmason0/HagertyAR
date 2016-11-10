677b69707b458470d141e09bbb7749374ff0d9e4
MASON <5917000863@student.myscps.us>
2016-11-10 14:22:35 -0500

    working on fading out

---
+++ HagertyAR_1.0/Assets/Dissappear_Reappear.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Dissappear_Reappear : MonoBehaviour {
+    Color colorStart;
+    Color colorEnd;
+    float duration = 1f;
+    bool fading = false;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+        colorStart = GetComponent<Renderer>().material.color;
+        colorEnd = new Color(colorStart.r, colorStart.g, colorStart.b, 0f);
+    }
+
+	void FadeOut ()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        fading = true;
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            for (int i = 0; i < renderers.Length; i++)
+                renderers[i].material.color = Color.Lerp(colorStart, colorEnd, t / duration);
+        }
+    }
+    // Update is called once per frame
+	void Update () {
+        Debug.Log("waiting");
+        if (Time.time > 10 && !fading)
+        {
+            FadeOut();
+            Debug.Log("should be gone");
+        }
+    }
+}
