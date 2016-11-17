a7a2fadcdf09644dc9e432ed3bf7bee86b129bd5
MASON <5917000863@student.myscps.us>
2016-11-17 14:22:30 -0500

    11/17

--- HagertyAR_1.0/Assets/Dissappear_Reappear.cs
+++ HagertyAR_1.0/Assets/Dissappear_Reappear.cs
@@ -2,36 +2,56 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
 
 public class Dissappear_Reappear : MonoBehaviour {
+    private struct Room
+    {
+        public string Number;
+        public GameObject Obj;
+
+        public Room(string n, GameObject o)
+        {
+            Number = n;
+            Obj = o;
+        }
+    }
+
+    private List<Room> roomList = new List<Room>();
+
     Color colorStart;
     Color colorEnd;
     float duration = 1f;
-    bool fading = false;
+    bool visible = true;
 	// Use this for initialization
 	void Start () {
-        Debug.Log("start");
-        colorStart = GetComponent<Renderer>().material.color;
-        colorEnd = new Color(colorStart.r, colorStart.g, colorStart.b, 0f);
-    }
+        //hide unnamed icons
+        GameObject[] hiddenIcons = GameObject.FindGameObjectsWithTag("Hide");
+        Debug.Log(hiddenIcons.Length+" icons to hide");
+        foreach (GameObject go in hiddenIcons)
+            go.SetActive(false);
 
-	void FadeOut ()
-    {
-        Renderer[] renderers = GetComponentsInChildren<Renderer>();
-        fading = true;
-        for (float t = 0f; t < duration; t += Time.deltaTime)
-        {
-            for (int i = 0; i < renderers.Length; i++)
-                renderers[i].material.color = Color.Lerp(colorStart, colorEnd, t / duration);
-        }
-    }
+        //create list of named icons
+        string[] numbers = new string[] {"Room 6-123", "Room 6-125", "Room 6-111", "Room 6-108", "Room 6-117B", "Room 6-117", "Room 6-124", "Room 6-103", "Room 6-116", "Room 6-114", "Room 6-100C", "Room 6-100", "Room 6-100A", "Room 6-100B", "Room 6-112", "Room 6-109", "Room 6-106A", "Room 6-106", "Room 6-102", "Room 6-101", "Room 6-113", "Room 7-102", "Room 7-107", "Room 7-111", "Room 7-112", "Room 7-110", "Room 7-103", "Room 7-100", "Room 7-114", "Room 7-117", "Room 7-108", "Room 7-124", "Room 7-124C", "Room 7-109", "Mechanical: 7-121", "Mechanical: 7-106", "Boys Restroom 7-118", "Boys Restroom 7-104", "Girls Restroom 7-119", "Girls Restroom 7-105", "Room 7-125", "Room 7-126", "Room 7-122A", "Room 7-122", "Room 7-120", "Room 7-116", "Room 7-115", "Room 7-100C", "Room 7-101", "Room 7-100B", "Room 7-100A"};
+        foreach (string number in numbers)
+	        roomList.Add(new Room(number, GameObject.Find(number)));
+
+        //hide named icons
+        foreach (Room room in roomList)
+            room.Obj.SetActive(false);
+}
+
+	
     // Update is called once per frame
 	void Update () {
-        //Debug.Log("waiting");
-        if (Time.time > 10 && !fading)
+        if(Time.time > 10f && visible)
         {
-            FadeOut();
-            Debug.Log("should be gone");
+            //GameObject.Find("ImageTarget/Bldg3SecondFloor").SetActive(false);
+            //Debug.Log("Bldg3SecondFloor hidden");
+            //visible = false;
         }
+        
     }
 }
--- HagertyAR_1.0/Assets/GUICode.cs
+++ HagertyAR_1.0/Assets/GUICode.cs
@@ -6,6 +6,7 @@ public class GUICode : MonoBehaviour {
     private bool mainMenu = true;
     private bool searchRooms = false;
     private bool searchLockers = false;
+    private string textInput = string.Empty;
     // Use this for initialization
     void Start () {
 	
@@ -24,8 +25,26 @@ public class GUICode : MonoBehaviour {
             GUI.Box(new Rect(5, 5, guiW, guiH), "Menu");
             if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height - 30, 80, 20), "Dismiss"))
                 visible = false;
-            if(mainMenu && GUI.Button(new Rect(Screen.width - 55, 5, 50, 20),"Search for a Room")) //fix size
+            if (mainMenu && GUI.Button(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 20, 120, 20), "Search for a Room"))
+            {
                 searchRooms = true;
+                mainMenu = false;
+            }
+            if (searchRooms)
+            {
+                textInput = GUI.TextField(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 20, 120, 20), textInput);
+                if (GUI.Button(new Rect(Screen.width / 2 - 25, Screen.height / 2 + 5, 50, 20), "Back"))
+                {
+                    searchRooms = false;
+                    mainMenu = true;
+                }
+                if (GUI.Button(new Rect(Screen.width / 2 + 30, Screen.height / 2 - 20, 20, 20), "Go"))
+                {
+                    mainMenu = true;
+                    searchRooms = false;
+                    visible = false;
+                }
+            }
         }
         else //Menu is hidden
         {
